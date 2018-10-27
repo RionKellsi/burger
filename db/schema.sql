016bdc730fef3983e4b873db243ac0b04e64116b
Rion <rionirvin>
2018-10-26 23:57:49 -0500

    style edits

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,10 @@
+
+
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
+
+DROP TABLE burgers;
 
 CREATE TABLE burgers(
   id INT AUTO_INCREMENT NOT NULL,
--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var exphbs = require("express-handlebars");
-
-// var PORT = process.env.PORT || 3000;
-
-// var app = express();
-
-// //TODO: make Public folder
-// app.use(express.static("./public"));
-
-// app.use(bodyParser.urlencoded({ extended: true}));
-
-// app.use(bodyParser.json());
-
-// //TODO: make handlebars folder with index and main pages
-// app.engine("handlebars", exphbs({ defaultLayout: "main"}));
-// app.set("view engine", "handlebars");
-
-// // TODO: make Controllers folder
-// var routes = require("./controllers/burger_controller.js");
-
-// app.use('/', routes);
-
-// app.listen(PORT, function(){
-//     console.log("server listening on: http://localhost: " + PORT );
-// });
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
